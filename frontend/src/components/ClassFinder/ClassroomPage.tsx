cc8c931d2b2235e547a87850f0a782df59f4c059
angelabk49 <angelabk0904@gmail.com>
2025-02-28 20:01:14 +0530

    classroom

---
+++ frontend/src/components/ClassFinder/ClassroomPage.tsx
@@ -0,0 +1,3 @@
+//main page that contains : list of blocks, week selector
+//in expo drop down that contains starting from current date to the end of the week(saturday)
+//four buttoons with block names that go to block id(same logic as announcments)
