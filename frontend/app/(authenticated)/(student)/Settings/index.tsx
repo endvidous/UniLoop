4ad073920f613fffc2ec9ba7b3143bc3cd34ded7
Henry-Endvidous <henrypunnoose2004@gmail.com>
2025-03-02 04:26:39 +0530

    Added pages to student to check

---
+++ frontend/app/(authenticated)/(student)/Settings/index.tsx
@@ -0,0 +1,6 @@
+import SettingsPage from "@/src/components/setting/SettingPage";
+const Settings = () => {
+  return <SettingsPage />;
+};
+
+export default Settings;
--- frontend/app/(authenticated)/(student)/_layout.tsx
+++ frontend/app/(authenticated)/(student)/_layout.tsx
@@ -1,5 +1,64 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function StudentLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
+const TabLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#0003a0",
+        tabBarHideOnKeyboard: true,
+        // headerBackButtonDisplayMode: "default",
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={28} name="home" color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="Announcements"
+        options={{
+          title: "Announcements",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={28} name="announcement" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Discussions"
+        options={{
+          title: "Discussions",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={28} name="chat" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Assignments"
+        options={{
+          title: "Assignments",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={28} name="assignment" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
